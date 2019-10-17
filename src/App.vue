e1f4bf2809b44e7b63ba6865180e906f266ea2a8
kingrockman <181013906@qq.com>
2019-10-18 01:25:29 +0800

    2019-10-18

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script>
+import { login, setData } from "./utils";
+
 export default {
-  created() {
+  async created() {
     if (!wx.cloud) {
       console.error("请使用 2.2.3 或以上的基础库以使用云能力");
     } else {
@@ -8,6 +10,7 @@ export default {
         traceUser: true
       });
     }
+
     // 调用API从本地缓存中获取数据
     /*
      * 平台 api 差异的处理方式:  api 方法统一挂载到 mpvue 名称空间, 平台判断通过 mpvuePlatform 特征字符串
@@ -69,13 +72,13 @@ input {
   border-top: pink solid 1px;
 }
 textarea {
-  width: 100%;  
+  width: 100%;
   padding: 10rpx;
   margin-bottom: 10rpx;
   border-bottom: pink solid 1px;
   border-top: pink solid 1px;
 }
-div{
+div {
   font-size: 24rpx;
 }
 </style>
--- src/utils.js
+++ src/utils.js
@@ -2,98 +2,117 @@
 // const conn = 'customers'
 
 function message(title) {
-    wx.showToast({
-        title: title,
-        mask: true
-    })
+  wx.showToast({
+    title: title,
+    mask: true
+  })
 }
 
 function setData(key, data) {
-    wx.setStorageSync(key, data)
+  wx.setStorageSync(key, data)
 }
 
 function getData(key) {
-    return wx.getStorageSync(key)
+  return wx.getStorageSync(key)
+}
+
+function login(conn, data) {
+  return new Promise((resolve, reject) => {
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {
+        conn,
+        data
+      },
+      success: (res) => {
+        resolve(res.result)
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    })
+  })
 }
 
 function create(conn, data) {
-    return new Promise((resolve, reject) => {
-        wx.cloud.callFunction({
-            name: 'create',
-            data: {
-                conn,
-                data
-            },
-            success: (res) => {
-                resolve(res.result)
-            },
-            fail: (err) => {
-                reject(err)
-            }
-        })
+  return new Promise((resolve, reject) => {
+    wx.cloud.callFunction({
+      name: 'create',
+      data: {
+        conn,
+        data
+      },
+      success: (res) => {
+        resolve(res.result)
+      },
+      fail: (err) => {
+        reject(err)
+      }
     })
+  })
 }
 
 function read(conn) {
 
-    return new Promise((resolve, reject) => {
-        wx.cloud.callFunction({
-            name: 'read',
-            data: {
-                conn
-            },
-            success: (res) => {
-                resolve(res.result)
-            },
-            fail: (err) => {
-                reject(err)
-            }
-        });
-    })
+  return new Promise((resolve, reject) => {
+    wx.cloud.callFunction({
+      name: 'read',
+      data: {
+        conn
+      },
+      success: (res) => {
+        resolve(res.result)
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    });
+  })
 }
 
 function update(conn, data) {
 
-    return new Promise((resolve, reject) => {
-        wx.cloud.callFunction({
-            name: 'update',
-            data: {
-                conn,
-                data
-            },
-            success: (res) => {
-                resolve(res.result)
-            },
-            fail: (err) => {
-                reject(err)
-            }
-        });
-    })
+  return new Promise((resolve, reject) => {
+    wx.cloud.callFunction({
+      name: 'update',
+      data: {
+        conn,
+        data
+      },
+      success: (res) => {
+        resolve(res.result)
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    });
+  })
 }
 
 function del(conn) {
 
-    return new Promise((resolve, reject) => {
-        wx.cloud.callFunction({
-            name: 'read',
-            data: {
-                conn
-            },
-            success: (res) => {
-                resolve(res.result)
-            },
-            fail: (err) => {
-                reject(err)
-            }
-        });
-    })
+  return new Promise((resolve, reject) => {
+    wx.cloud.callFunction({
+      name: 'read',
+      data: {
+        conn
+      },
+      success: (res) => {
+        resolve(res.result)
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    });
+  })
 }
 export {
-    create,
-    read,
-    update,
-    del,
-    message,
-    setData,
-    getData
+  login,
+  create,
+  read,
+  update,
+  del,
+  message,
+  setData,
+  getData
 }
