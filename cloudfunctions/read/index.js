b2bc7e4c23b507575afc67e30500f1203783920c
kingrockman <181013906@qq.com>
2019-11-27 19:20:15 +0800

    优化云函数（read）功能

--- cloudfunctions/read/index.js
+++ cloudfunctions/read/index.js
@@ -4,25 +4,27 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 // 云函数入口函数
-exports.main = async(event) => {
-  const db = cloud.database();
+exports.main = async (event) => {
+  const db = cloud.database()
+  var len = {
+    skip: 100,
+    limit: 100
+  }
   const conn = event.conn
   const data = event.data
-  const skip = limit = 100
-  var newList = []
-  var res = await db.collection(conn).where(data).count()
-  var j = res.total / skip
-  console.log(Math.floor(j))
+  // len.skip = len.limit = 100
+  // var newList = []
+  var {
+    total: count
+  } = await db.collection(conn).where(data).count()
+  var j = Math.floor(count / len.skip)
+  // console.log(Math.floor(j))
+  return j
 
-  for (let i = 0; i < j; i++) {
-    var obj = await db.collection(conn).where(data)
-      .skip(i * skip).limit(limit).get()
-    newList.push(...obj.data)
-  }
-  // console.log(newList)
-  return newList
-  // console.log("总个数：", ct)
-  // try {
-  //     return db.collection(conn).where(data).get()
-  // } catch (e) {}
+  // for (let i = 0; i < j; i++) {
+  //   var obj = await db.collection(conn).where(data)
+  //     .skip(i * len.skip).limit(len.limit).get()
+  //   newList.push(...obj.data)
+  // }
+  // return newList
 }
