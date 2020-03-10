041dfcb49c6663e3d1f6cb34991d234e325c99ae
陈浩 <181013906@qq.com>
2020-03-10 10:23:07 +0800

    20200310

---
+++ src/pages/knowledge/Knowledge.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>知识库</div>
+</template>
+<script>
+export default {
+  data() {}
+};
+</script>
+<style scoped></style>
---
+++ src/pages/knowledge/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import Knowledge from './Knowledge'
+
+const app = new Vue(Knowledge)
+app.$mount()
